7e7532d8430bf60308fdfc4791db9049337cacb2
denisleuta <146765040+denisleuta@users.noreply.github.com>
2024-03-14 19:52:40 +0300

    Обновил BMPGenerator.h
    Изменение структуры вершин добавлением их номера

--- BMPGenerator.h
+++ BMPGenerator.h
@@ -6,6 +6,7 @@ struct Vertex {
     int x;
     int y;
     std::string label;
+    int tableNumber;
 };
 
 struct Edge {
@@ -16,22 +17,21 @@ struct Edge {
 class BMPGenerator {
 public:
     BMPGenerator(int width, int height, const std::vector<Vertex>& vertices, const std::vector<Edge>& edges);
-
+    void drawCharacter(std::vector<std::vector<bool>>& bitmap, char character, int x, int y);
     void generate(const std::string& filename);
-
+    void drawCircle(std::vector<std::vector<bool>>& bitmap, int xc, int yc);
+    void drawText(std::vector<std::vector<bool>>& bitmap, int tableNumber, int x, int y);
 private:
     void writeHeader(std::ofstream& file);
     void writeImageData(std::ofstream& file);
-    void drawText(std::vector<std::vector<bool>>& bitmap, const std::string& text, int x, int y);
-    void drawCharacter(std::vector<std::vector<bool>>& bitmap, char character, int x, int y);
     void drawLine(std::vector<std::vector<bool>>& bitmap, int x0, int y0, int x1, int y1);
-    void drawCircle(std::vector<std::vector<bool>>& bitmap, int xc, int yc);
     void writeInt(std::ofstream& file, int value);
     void writeShort(std::ofstream& file, short value);
+    
 private:
     int m_width;
     int m_height;
     std::vector<Vertex> m_vertices;
     std::vector<Edge> m_edges;
-    void readInputFromFile(const std::string& filename, int& numVertices, int& width, int& height, std::vector<Edge>& edges);
+    std::vector<std::vector<std::vector<bool>>> alpha;
 };
