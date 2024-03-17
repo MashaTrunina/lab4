da5a3d59cb9d13f058140ea94c41c7eaec61fe7e
denisleuta <146765040+denisleuta@users.noreply.github.com>
2024-03-17 23:02:40 +0300

    Update f.cpp
    Оптимизировал код

--- f.cpp
+++ f.cpp
@@ -1,9 +1,6 @@
+#include <cstdlib>
 #include "BMPGenerator.h"
-#include <iostream>
-#include <vector>
-#include <ctime>
-#include <fstream>
-#include <sstream>
+#include "FileReader.h"
 
 template<typename T>
 std::string to_string(T value) {
@@ -12,33 +9,20 @@ std::string to_string(T value) {
     return os.str();
 }
 
-void readInputFromFile(const std::string& filename, int& numVertices, int& width, int& height, std::vector<Edge>& edges) {
-    std::ifstream file(filename);
-    if (file.is_open()) {
-        file >> numVertices >> width >> height;
-
-        int vertex1, vertex2;
-        while (file >> vertex1 >> vertex2) {
-            edges.push_back({ static_cast<size_t>(vertex1), static_cast<size_t>(vertex2) });
-        }
-
-        file.close();
-    }
-    else {
-        std::cerr << "Unable to open file: " << filename << std::endl;
-    }
-}
-
 int main() {
     int numVertices, width, height;
-    std::vector<Edge> edges;
-    readInputFromFile("input.txt", numVertices, width, height, edges);
+    readInputFromFile("graphs_data.txt", numVertices, width, height);
 
     std::vector<Vertex> vertices;
-    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+    std::vector<Edge> edges;
+
+    srand(time(NULL));
+
     for (int i = 0; i < numVertices; ++i) {
-        vertices.push_back({ std::rand() % width, std::rand() % height, to_string(i), i }); 
+        vertices.push_back({ rand() % width, rand() % height, to_string(i) });
     }
+
+    readEdgesFromFile("edge_data.txt", edges);
 
     BMPGenerator bmpGenerator(width, height, vertices, edges);
     bmpGenerator.generate("graph.bmp");
