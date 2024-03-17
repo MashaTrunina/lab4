e59251221da7585187cb8451cf7b259b67014dc0
denisleuta <146765040+denisleuta@users.noreply.github.com>
2024-03-17 23:01:19 +0300

    Update BMPGenerator.h
    Оптимизировал код

--- BMPGenerator.h
+++ BMPGenerator.h
@@ -1,37 +1,34 @@
 #pragma once
+#include <iostream>
+#include <fstream>
 #include <vector>
-#include <string>
-
-struct Vertex {
-    int x;
-    int y;
-    std::string label;
-    int tableNumber;
-};
-
-struct Edge {
-    size_t vertex1;
-    size_t vertex2;
-};
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
+#include <sstream>
+#include "Vertex.h"
+#include "Edge.h"
 
 class BMPGenerator {
 public:
     BMPGenerator(int width, int height, const std::vector<Vertex>& vertices, const std::vector<Edge>& edges);
-    void drawCharacter(std::vector<std::vector<bool>>& bitmap, char character, int x, int y);
+
     void generate(const std::string& filename);
-    void drawCircle(std::vector<std::vector<bool>>& bitmap, int xc, int yc);
-    void drawText(std::vector<std::vector<bool>>& bitmap, int tableNumber, int x, int y);
+
 private:
     void writeHeader(std::ofstream& file);
     void writeImageData(std::ofstream& file);
+    void drawText(std::vector<std::vector<bool>>& bitmap, const std::string& text, int x, int y);
+    void drawCharacter(std::vector<std::vector<bool>>& bitmap, char character, int x, int y);
     void drawLine(std::vector<std::vector<bool>>& bitmap, int x0, int y0, int x1, int y1);
+    void drawCircle(std::vector<std::vector<bool>>& bitmap, int xc, int yc);
     void writeInt(std::ofstream& file, int value);
     void writeShort(std::ofstream& file, short value);
-    
+
 private:
     int m_width;
     int m_height;
     std::vector<Vertex> m_vertices;
     std::vector<Edge> m_edges;
-    std::vector<std::vector<std::vector<bool>>> alpha;
 };
+
